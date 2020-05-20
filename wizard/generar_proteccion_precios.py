3e7fb48dadd9ffb4542c02bb33f52d2b58322d08
Rodrigo Fernandez <rodrigo@solucionesprisma.com>
2020-05-20 01:44:01 +0000

    Generar partida

--- wizard/generar_proteccion_precios.py
+++ wizard/generar_proteccion_precios.py
@@ -15,19 +15,24 @@ class IntcomexGenerarProteccionPreciosWizard(models.TransientModel):
     fecha_fin = fields.Date(string='Fecha fin', required=True)
 
     def crear_partida_contable(self):
+
         facturas = self.env['account.move'].search([('type', 'in', ['out_invoice', 'out_refund']), ('state', '=', 'posted'), ('date', '>=', self.fecha_inicio), ('date', '<=', self.fecha_fin), ('partida_intcomex', '!=', True)])
         cuenta_inventario_ids = {}
         #En este ciclo se crea un diccionario con todas las cuentas de inventario que serán utilizadas para crear la partida contable, y su respectiva sumatoria del monto.
         for factura in facturas:
             logging.warn(factura)
-            factura.partida_intcomex = True
+            logging.warn(factura._get_invoiced_lot_values())
+#            factura.partida_intcomex = True
             for linea in factura.invoice_line_ids:
                 if not linea.product_id.categ_id:
                     raise UserError("El producto '" + linea.product_id.name + "' no tiene definida una categoría.")
                 if not linea.product_id.categ_id.property_stock_valuation_account_id:
                     raise UserError("La categoría de producto '" + linea.product_id.categ_id.name + "' no tiene definida una cuenta contable de inventario.")
 
-                monto_proteccion = linea.obtener_proteccion(self.fecha_inicio,self.fecha_fin)
+                monto_proteccion = self.obtener_proteccion(linea)
+                sale_orders = linea.mapped('sale_line_ids.order_id')
+                stock_move_lines = sale_orders.mapped('picking_ids.move_lines.move_line_ids')
+
                 if monto_proteccion > 0:
                     if factura.type == 'out_refund':
                         monto_proteccion *= -1
